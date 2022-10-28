02432e00d306266fa1c17afd80b8580c7d7c2564
Константин <kozzztia@ukr.net>
2022-10-28 19:14:37 +0300

    isauthHoc

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, {FC, useState} from 'react';
 import './App.css';
 import {
     Routes,
-    Route
+    Route, Navigate
 } from "react-router-dom";
 import PageLayout from "./components/PageLayout";
 import MainLayout from "./components/MainLayout";
 import ItemComponent from "./components/ItemComponent";
 
+
+interface Interface {
+    isAuth: boolean,
+    children : React.ReactNode | React.ReactNode[],
+}
+
+
+const IsAuthHok = ({isAuth , children}:Interface) : any => {
+    if(!isAuth){
+        return <><Navigate  to={"/login"}/></>
+    }
+    return children
+}
+
+
+
+
 function App() {
-  return (
+    const [isAuth , setIsAuth] = useState(false)
+
+    return (
     <div className="App">
 
      <Routes>
          <Route path={"/"} element={<PageLayout/>}>
             <Route path={"*"} element={<p>404</p>}/>
             <Route path={"login"} element={<p>login</p>}/>
-             <Route path={"main"} element={<MainLayout/>}/>
-             <Route path={"/main/:id"} element={<ItemComponent/>}/>
+             <Route path={"main"} element={
+                 <IsAuthHok isAuth={isAuth}>
+                     <Route index element={<MainLayout/>}/>
+                     <Route path={"/main/:id"} element={<ItemComponent/>}/>
+                 </IsAuthHok>
+
+             }/>
+
+
+
          </Route>
 
      </Routes>
